531cf41660745c5253e4637cf184b991c1e78e2e
MrSeaWave <774866545@qq.com>
2018-12-07 16:01:36 +0800

    使用原型上的isReactComponent去判断

--- src/DiffClassAndFunction/index.js
+++ src/DiffClassAndFunction/index.js
@@ -12,6 +12,9 @@ class DiffClassAndFunction extends Component{
         实例化函数
         {ReactClass.prototype instanceof Component ?'react.class':'no react.class'}
         <br/>
+        使用isReactComponent判断
+        {ReactClass.prototype.isReactComponent?'react.component':'no'}
+        <br/>
         {ReactArrowFunction.prototype instanceof Component ?'react.class':'箭头函数没有原型'}
         <br/>
         {ReactFunction.prototype instanceof Component ?'react.class':'普通函数不是react.class'}
