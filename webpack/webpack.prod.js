e57ea2b4b9d23991f210a46bcf3f81498d24a447
MrSeaWave <774866545@qq.com>
2018-12-13 21:04:57 +0800

    comments

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -2,7 +2,8 @@ const webpack = require('webpack');
 const merge = require('webpack-merge');
 const common = require('./webpack.common');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
-// TODO 打包运行出错，待修改
+// 打包
+// 直接在dist 文件夹下运行`live-server` or `live-server --open=index.html`
 module.exports = merge(common, {
   mode: 'production',
   devtool: 'source-map',
